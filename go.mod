0bf076739bf37f443f8f91c824747c4ffe12b6a4
Petr Karmashev <pakarmashev@avito.ru>
2021-08-25 04:54:04 +0300

    SNP500 prices fetching

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/vehsamrak/stocks
+
+go 1.16
+
+require (
+	github.com/extrame/ole2 v0.0.0-20160812065207-d69429661ad7 // indirect
+	github.com/extrame/xls v0.0.1
+	go.uber.org/ratelimit v0.2.0
+)
---
+++ snp_price_fetcher.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/extrame/xls"
+	"go.uber.org/ratelimit"
+)
+
+const (
+	rateLimitInSeconds = 1
+	// Export link from https://www.spglobal.com/spdji/en/indices/equity/sp-500/#overview
+	urlSNP500              = "https://www.spglobal.com/spdji/en/idsexport/file.xls?redesignExport=true&languageId=1&selectedModule=PerformanceGraphView&selectedSubModule=Graph&yearFlag=tenYearFlag&indexId=340"
+	urlTimeout             = time.Second * 30
+	urlTLSHandshakeTimeout = time.Second * 5
+	stockName              = "S&P 500"
+	rowStockName           = 6
+	columnDate             = 0
+	columnPrice            = 1
+	SNP500PricesFileName   = "spx_prices.xls"
+)
+
+type Price struct {
+	Date  time.Time
+	Price float64
+}
+
+// note: this is off by two days on the real epoch (1/1/1900) because
+// - the days are 1 indexed so 1/1/1900 is 1 not 0
+// - Excel pretends that Feb 29, 1900 existed even though it did not
+// The following function will fail for dates before March 1st 1900
+// Before that date the Julian calendar was used so a conversion would be necessary
+var excelEpoch = time.Date(1899, time.December, 30, 0, 0, 0, 0, time.UTC)
+
+func main() {
+	rateLimiter := ratelimit.New(rateLimitInSeconds)
+	prices, err := downloadPrices(rateLimiter, urlSNP500)
+	if err != nil {
+		panic(err)
+	}
+
+	printPrices(prices)
+}
+
+func downloadPrices(rateLimiter ratelimit.Limiter, pricesUrl string) (*[]Price, error) {
+	rateLimiter.Take()
+
+	var netClient = &http.Client{
+		Timeout: urlTimeout,
+		Transport: &http.Transport{
+			DialContext: (&net.Dialer{
+				Timeout:   urlTimeout,
+				KeepAlive: urlTimeout,
+			}).DialContext,
+			TLSHandshakeTimeout: urlTLSHandshakeTimeout,
+		},
+	}
+	response, _ := netClient.Get(pricesUrl)
+	defer response.Body.Close()
+
+	responseBodyBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO[petr]: save file with DATE. Do not download file if date of last file is today
+	filename := SNP500PricesFileName
+	err = ioutil.WriteFile(filename, responseBodyBytes, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	prices, err := fetchPricesFromXLS(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	return prices, nil
+}
+
+func fetchPricesFromXLS(xlsFilePath string) (*[]Price, error) {
+	table, err := xls.Open(xlsFilePath, "UTF8")
+	if err != nil {
+		return nil, err
+	}
+
+	sheet := table.GetSheet(0)
+
+	err = validateSheet(sheet)
+	if err != nil {
+		return nil, err
+	}
+
+	currentRow := rowStockName + 1
+	maxLoopSize := 1000000
+	var prices []Price
+	for {
+		if currentRow > maxLoopSize {
+			return nil, fmt.Errorf("maximum loop size reached")
+		}
+
+		dateValue := sheet.Row(currentRow).Col(columnDate)
+		priceValue := sheet.Row(currentRow).Col(columnPrice)
+
+		if dateValue == "" {
+			break
+		}
+
+		price, err := strconv.ParseFloat(priceValue, 64)
+		prices = append(
+			prices, Price{
+				Date:  convertExcelDate(dateValue),
+				Price: price,
+			},
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		currentRow++
+	}
+
+	return &prices, nil
+}
+
+func validateSheet(sheet *xls.WorkSheet) error {
+	row6Column1 := sheet.Row(rowStockName).Col(columnPrice)
+	if row6Column1 != stockName {
+		return fmt.Errorf("row 6 column 1 must be \"%s\". Current is \"%s\"", stockName, row6Column1)
+	}
+
+	return nil
+}
+
+func printPrices(prices *[]Price) {
+	for _, price := range *prices {
+		fmt.Printf("%s - %.2f\n", price.Date.Format("02.01.2006"), price.Price)
+	}
+}
+
+func convertExcelDate(excelDate string) time.Time {
+	var days, _ = strconv.Atoi(excelDate)
+	return excelEpoch.Add(time.Second * time.Duration(days*86400))
+}
